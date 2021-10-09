4a6ea5dc3951074cdb4d2bf5d8fef916eae8bef7
David Pallarés <serallapdivad@gmail.com>
2021-10-09 11:04:34 +0100

    Change JS stuff to do

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
-// JavaScript to be addedj iuhbfw
+// JavaScript to be added
+
+// To do!
+// Open the project and make sure that after the user enters their email, the button changes its text to say, "Thanks for your email!"
