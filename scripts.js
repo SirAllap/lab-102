c81c6148c66b64f1bf56c633ea3bc20504d4b5cd
David Pallarés <serallapdivad@gmail.com>
2021-10-10 13:52:04 +0100

    I added the function where the user can add an email modifiying .js and .html

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
-<<<<<<< HEAD
-// JavaScript to be added
-
-// To do!
-// Open the project and make sure that after the user enters their email, the button changes its text to say, "Thanks for your email!"
-=======
-// JavaScript to be adde
-//Modificado
->>>>>>> a43bf0e2eb6edffedb1a9948e61811d997748d83
+function messagePrompt() {
+    //Show message
+    var email = prompt("Write your email here", "");
+    //Check for email
+        if (email != null && email != ""){
+            alert("Thanks for your email!");
+        }
+        //Check if user press cancel
+        else if (email === null){
+            alert("You can come anytime!")
+        }
+        //Check if user didn't put a email 
+        else {
+            alert("You didn't write any email!");
+            email = prompt("Write your email here", "")
+            if (email != null && email != "") {
+                alert("Thanks for your email!");
+            } else {
+                alert("You can come anytime!")
+            }
+        }
+    }
